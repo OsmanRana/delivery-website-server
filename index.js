19ade0db48ca608e26211bb09bf8c9877b60d357
OsmanRana <osmancmc@yahoo.com>
2022-02-04 11:09:32 +0600

    env file added

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+//user: deliveryWebsite
+//password: Yecxz585JQjudGWR
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://${process.env.DB_USER}:${process.evn.DB_PASS}@cluster0.x13qi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("hitting the database");
+  client.close();
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World xyz");
+  res.send("Hello World from express delivery server");
 });
 
 app.listen(port, () => {
