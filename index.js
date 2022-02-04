1f531a76a9ce205a4a0dade9dc27a7810a0f9a92
OsmanRana <osmancmc@yahoo.com>
2022-02-04 11:58:14 +0600

    services send to db

--- index.js
+++ index.js
@@ -5,29 +5,38 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-//user: deliveryWebsite
-//password: Yecxz585JQjudGWR
-
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://${process.env.DB_USER}:${process.evn.DB_PASS}@cluster0.x13qi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x13qi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("hitting the database");
-  client.close();
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World from express delivery server");
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("expressDelivery");
+    const servicesCollection = database.collection("services");
+
+    //send all services GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Listening to port ", port);
